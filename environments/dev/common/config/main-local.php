d51991c8698bbf6ff2d488ec16346e5d2bb14782
Vishal_bhalani <vishalvbhalani@gmail.com>
2016-09-22 11:59:32 +0530

    Change Mail configuration

--- environments/dev/common/config/main-local.php
+++ environments/dev/common/config/main-local.php
@@ -24,8 +24,8 @@ return [
          'transport' => [
              'class' => 'Swift_SmtpTransport',
              'host' => 'smtp.gmail.com',  // e.g. smtp.mandrillapp.com or smtp.gmail.com
-             'username' => 'vcodertechnolab@gmail.com',
-             'password' => 'vcoder@2016',
+             'username' => 'test.vcoderteam@gmail.com',
+             'password' => 'vcoderteam',
              'port' => '587', // Port 25 is a very common port too
              'encryption' => 'tls', // It is often used, check your provider or mail server specs
          ],
