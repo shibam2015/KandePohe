5cc32d3c4bee7478dc936de06b4da9de4e4442c1
VikrantParmar <parmarvikrantr@gmail.com>
2017-01-23 19:05:50 +0530

    - Set Dropdown in My profile Page under preferences section - Set validation

--- frontend/views/user/_preferences.php
+++ frontend/views/user/_preferences.php
@@ -3,172 +3,174 @@ use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 use common\components\CommonHelper;
 use yii\helpers\ArrayHelper;
-
 ?>
-    <style>
-        input[type="radio"], input[type="checkbox"] {
-            display: inline-block;
-        }
+<style>
+    input[type="radio"], input[type="checkbox"] {
+        display: inline-block;
+    }
 
-        input[type="radio"]:checked + label::before {
-            content: "";
-        }
-    </style>
+    input[type="radio"]:checked + label::before {
+        content: "";
+    }
+</style>
 <div class="div_preferences">
-<?php
-if ($show) {
-    $form = ActiveForm::begin([
-        'id' => 'form',
-        'action' => ['edit-preferences'],
-        'options' => ['data-pjax' => true],
-        'layout' => 'horizontal',
-        'validateOnChange' => false,
-        'validateOnSubmit' => true,
-        'fieldConfig' => [
-            'template' => "{label}{beginWrapper}\n{input}\n{hint}\n{endWrapper}",
-            'horizontalCssClasses' => [
-                'label' => 'col-sm-3 col-xs-3',
-                'offset' => '',
-                'wrapper' => 'col-sm-8 col-xs-8',
-                'error' => '',
-                'hint' => '',
+    <?php
+    if ($show) {
+        $form = ActiveForm::begin([
+            'id' => 'form',
+            'action' => ['edit-preferences'],
+            'options' => ['data-pjax' => true],
+            'layout' => 'horizontal',
+            'validateOnChange' => false,
+            'validateOnSubmit' => true,
+            'fieldConfig' => [
+                'template' => "{label}{beginWrapper}\n{input}\n{hint}\n{endWrapper}",
+                'horizontalCssClasses' => [
+                    'label' => 'col-sm-3 col-xs-3',
+                    'offset' => '',
+                    'wrapper' => 'col-sm-8 col-xs-8',
+                    'error' => '',
+                    'hint' => '',
+                ]
             ]
-        ]
-    ]);
-    ?>
-    <?= $form->errorSummary([$PartenersReligion],['header' => '<p>Oops! Please ensure all fields are valid</p>']); ?>
+        ]);
+        ?>
+        <?= $form->errorSummary([$PartenersReligion], ['header' => '<p>Oops! Please ensure all fields are valid</p>']); ?>
         <?= $form->field($PartenersReligion, 'iReligion_ID')->dropDownList(
             ArrayHelper::map(CommonHelper::getReligion(), 'iReligion_ID', 'vName'),
-            ['prompt' => 'Religion']
+            ['class' => 'demo-default select-beast', 'prompt' => 'Religion']
         ); ?>
         <?php
-    $heightrange = range(134, 204);
-            $range = range(18, 100);
+        $heightrange = range(134, 204);
+        $range = range(18, 100);
         ?>
         <?= $form->field($UPP, 'age_from')->dropDownList(
             array_combine($range, $range),
-            ['prompt' => 'Age From']
+            ['class' => 'demo-default select-beast', 'prompt' => 'Age From']
         ); ?>
 
         <?= $form->field($UPP, 'age_to')->dropDownList(
             array_combine($range, $range),
-            ['prompt' => 'Age To']
+            ['class' => 'demo-default select-beast', 'prompt' => 'Age To']
         ); ?>
-    <?= $form->field($UPP, 'height_from')->dropDownList(
-        ArrayHelper::map(CommonHelper::getHeight(), 'iHeightID', 'vName'),
-        ['prompt' => 'Height From']
-    ); ?>
-    <?= $form->field($UPP, 'height_to')->dropDownList(
-        ArrayHelper::map(CommonHelper::getHeight(), 'iHeightID', 'vName'),
-        ['prompt' => 'Height To']
-    ); ?>
+        <?= $form->field($UPP, 'height_from')->dropDownList(
+            ArrayHelper::map(CommonHelper::getHeight(), 'iHeightID', 'vName'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Height From']
+        ); ?>
+        <?= $form->field($UPP, 'height_to')->dropDownList(
+            ArrayHelper::map(CommonHelper::getHeight(), 'iHeightID', 'vName'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Height To']
+        ); ?>
 
 
-    <!-- <?= $form->field($PartnersMaritalStatus, 'iMarital_Status_ID')->dropDownList(
-        ArrayHelper::map(CommonHelper::getMaritalStatus(), 'iMaritalStatusID', 'vName'),
-        ['prompt' => 'Maritial Status']
-    ); ?>-->
+        <!-- <?= $form->field($PartnersMaritalStatus, 'iMarital_Status_ID')->dropDownList(
+            ArrayHelper::map(CommonHelper::getMaritalStatus(), 'iMaritalStatusID', 'vName'),
+            ['prompt' => 'Maritial Status']
+        ); ?>-->
 
         <?= $form->field($PartnersMaritalStatus, 'iMarital_Status_ID')->dropDownList(
             ArrayHelper::map(CommonHelper::getMaritalStatus(), 'iMaritalStatusID', 'vName'),
-            ['prompt' => 'Maritial Status']
+            ['class' => 'demo-default select-beast', 'prompt' => 'Maritial Status']
         ); ?>
 
-    <?= $form->field($PartnersMothertongue, 'iMothertongue_ID')->dropDownList(
-        ArrayHelper::map(CommonHelper::getMotherTongue(), 'ID', 'Name'),
-        ['prompt' => 'Mother Toungue']
+        <?= $form->field($PartnersMothertongue, 'iMothertongue_ID')->dropDownList(
+            ArrayHelper::map(CommonHelper::getMotherTongue(), 'ID', 'Name'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Mother Toungue']
         ); ?>
 
-    <?= $form->field($PartnersCommunity, 'iCommunity_ID')->dropDownList(
-        ArrayHelper::map(CommonHelper::getCommunity(), 'iCommunity_ID', 'vName'),
-        ['prompt' => 'Community']
-    ); ?>
-    <?= $form->field($PartnersSubCommunity, 'iSub_Community_ID')->dropDownList(
-        ArrayHelper::map(CommonHelper::getSubCommunity(), 'iSubCommunity_ID', 'vName'),
-        ['prompt' => 'Sub Community']
-    ); ?>
+        <?= $form->field($PartnersCommunity, 'iCommunity_ID')->dropDownList(
+            ArrayHelper::map(CommonHelper::getCommunity(), 'iCommunity_ID', 'vName'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Community']
+        ); ?>
+        <?= $form->field($PartnersSubCommunity, 'iSub_Community_ID')->dropDownList(
+            ArrayHelper::map(CommonHelper::getSubCommunity(), 'iSubCommunity_ID', 'vName'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Sub Community']
+        ); ?>
 
-    <?= $form->field($PartnersRaashi, 'raashi_id')->dropDownList(
-        ArrayHelper::map(CommonHelper::getRaashi(), 'ID', 'Name'),
-        ['prompt' => 'Raashi']
-    ); ?>
+        <?= $form->field($PartnersRaashi, 'raashi_id')->dropDownList(
+            ArrayHelper::map(CommonHelper::getRaashi(), 'ID', 'Name'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Raashi']
+        ); ?>
 
-    <?= $form->field($PartnersCharan, 'charan_id')->dropDownList(
-        ArrayHelper::map(CommonHelper::getCharan(), 'ID', 'Name'),
-        ['prompt' => 'Charan']
-    ); ?>
-    <?= $form->field($PartnersNakshtra, 'nakshtra_id')->dropDownList(
-        ArrayHelper::map(CommonHelper::getNaksatra(), 'ID', 'Name'),
-        ['prompt' => 'Nakshtra']
-    ); ?>
-    <?= $form->field($PartnersNadi, 'nadi_id')->dropDownList(
-        ArrayHelper::map(CommonHelper::getNadi(), 'ID', 'Name'),
-        ['prompt' => 'Nadi']
-    ); ?>
-    <?= $form->field($PartnersGotra, 'iGotra_ID')->dropDownList(
-        ArrayHelper::map(CommonHelper::getGotra(), 'iGotraID', 'vName'),
-        ['prompt' => 'Gotra']
-    ); ?>
+        <?= $form->field($PartnersCharan, 'charan_id')->dropDownList(
+            ArrayHelper::map(CommonHelper::getCharan(), 'ID', 'Name'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Charan']
+        ); ?>
+        <?= $form->field($PartnersNakshtra, 'nakshtra_id')->dropDownList(
+            ArrayHelper::map(CommonHelper::getNaksatra(), 'ID', 'Name'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Nakshtra']
+        ); ?>
+        <?= $form->field($PartnersNadi, 'nadi_id')->dropDownList(
+            ArrayHelper::map(CommonHelper::getNadi(), 'ID', 'Name'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Nadi']
+        ); ?>
+        <?= $form->field($PartnersGotra, 'iGotra_ID')->dropDownList(
+            ArrayHelper::map(CommonHelper::getGotra(), 'iGotraID', 'vName'),
+            ['class' => 'demo-default select-beast', 'prompt' => 'Gotra']
+        ); ?>
 
 
-    <?= $form->field($UPP, 'manglik')->RadioList(
-        ['Yes' => 'Yes', 'No' => 'No'],
-        [
-            'item' => function ($index, $label, $name, $checked, $value) {
-                $checked = ($checked) ? 'checked' : '';
-                $return = '<input type="radio" class = "mangalik" id="' . $value . '" name="' . $name . '" value="' . $value . '" ' . $checked . '>';
-                $return .= '<label for="' . $value . '">' . ucwords($label) . '</label>';
-                return $return;
-            }
-        ]
-    )
-    ?>
+        <?= $form->field($UPP, 'manglik')->RadioList(
+            ['Yes' => 'Yes', 'No' => 'No'],
+            [
+                'item' => function ($index, $label, $name, $checked, $value) {
+                    $checked = ($checked) ? 'checked' : '';
+                    $return = '<input type="radio" class = "mangalik" id="' . $value . '" name="' . $name . '" value="' . $value . '" ' . $checked . '>';
+                    $return .= '<label for="' . $value . '">' . ucwords($label) . '</label>';
+                    return $return;
+                }
+            ]
+        )
+        ?>
 
-    <?= $form->field($UPP, 'drink')->RadioList(
-        ['Yes' => 'Yes', 'No' => 'No', 'Occasionally' => 'Occasionally'],
-        [
-            'item' => function ($index, $label, $name, $checked, $value) {
-                $checked = ($checked) ? 'checked' : '';
-                $return = '<input type="radio" class = "drink" id="' . $value . '" name="' . $name . '" value="' . $value . '" ' . $checked . '>';
-                $return .= '<label for="' . $value . '">' . ucwords($label) . '</label>';
-                return $return;
-            }
-        ]
-    )
-    ?>
-    <?= $form->field($UPP, 'smoke')->RadioList(
-        ['Yes' => 'Yes', 'No' => 'No', 'Occasionally' => 'Occasionally'],
-        [
-            'item' => function ($index, $label, $name, $checked, $value) {
-                $checked = ($checked) ? 'checked' : '';
-                $return = '<input type="radio" class = "smoke" id="' . $value . '" name="' . $name . '" value="' . $value . '" ' . $checked . '>';
-                $return .= '<label for="' . $value . '">' . ucwords($label) . '</label>';
-                return $return;
-            }
-        ]
-    )
-    ?>
-    <div class="row">
-        <div class="col-md-4 col-md-offset-2">
-            <div class="form-cont">
+        <?= $form->field($UPP, 'drink')->RadioList(
+            ['Yes' => 'Yes', 'No' => 'No', 'Occasionally' => 'Occasionally'],
+            [
+                'item' => function ($index, $label, $name, $checked, $value) {
+                    $checked = ($checked) ? 'checked' : '';
+                    $return = '<input type="radio" class = "drink" id="' . $value . '" name="' . $name . '" value="' . $value . '" ' . $checked . '>';
+                    $return .= '<label for="' . $value . '">' . ucwords($label) . '</label>';
+                    return $return;
+                }
+            ]
+        )
+        ?>
+        <?= $form->field($UPP, 'smoke')->RadioList(
+            ['Yes' => 'Yes', 'No' => 'No', 'Occasionally' => 'Occasionally'],
+            [
+                'item' => function ($index, $label, $name, $checked, $value) {
+                    $checked = ($checked) ? 'checked' : '';
+                    $return = '<input type="radio" class = "smoke" id="' . $value . '" name="' . $name . '" value="' . $value . '" ' . $checked . '>';
+                    $return .= '<label for="' . $value . '">' . ucwords($label) . '</label>';
+                    return $return;
+                }
+            ]
+        )
+        ?>
+        <div class="row">
+            <div class="col-md-4 col-md-offset-2">
                 <div class="form-cont">
-                    <input type="hidden" name="save" value="1">
-                    <?= Html::submitButton('save', ['class' => 'btn btn-primary my-profile-sc-button preferences_submit', 'data-loading-text' => '<i class="fa fa-circle-o-notch fa-spin"></i> Saving', 'name' => 'register5']) ?>
+                    <div class="form-cont">
+                        <input type="hidden" name="save" value="1">
+                        <?= Html::submitButton('save', ['class' => 'btn btn-primary my-profile-sc-button preferences_submit', 'data-loading-text' => '<i class="fa fa-circle-o-notch fa-spin"></i> Saving', 'name' => 'register5']) ?>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="form-cont">
+                    <div class="form-cont">
+                        <?= Html::Button('Cancel', ['class' => 'btn btn-primary my-profile-sc-button', 'id' => 'cancel_edit_preferences', 'name' => 'cancel']) ?>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="col-md-4">
-            <div class="form-cont">
-                <div class="form-cont">
-                    <?= Html::Button('Cancel', ['class' => 'btn btn-primary my-profile-sc-button', 'id' => 'cancel_edit_preferences', 'name' => 'cancel']) ?>
-                </div>
-            </div>
-        </div>
-    </div>
-    <?php ActiveForm::end();
-} else {
-    ?>
-   
+        <?php ActiveForm::end();
+        $this->registerJs('
+          selectbox();
+         ');
+    } else {
+        ?>
+
         <dl class="dl-horizontal">
             <dt>Religion</dt>
             <dd><?= CommonHelper::setInputVal($PartenersReligion->religionName->vName,'text') ?><dd>
@@ -218,8 +220,8 @@ if ($show) {
             <dd><?= CommonHelper::setInputVal($UPP->smoke, 'text') ?>
             <dd>
         </dl>
-  
+
     <?php
-}
-?>
-  </div>
+    }
+    ?>
+</div>
